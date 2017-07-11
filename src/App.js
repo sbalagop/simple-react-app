b7f9a1e0865c46bd2d242cd5f6c24fb255f7eee7
Sivakumar Balagopalan <sivakumar.balagopalan@oracle.com>
2017-07-10 20:52:46 -0700

    transform scale text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          How are you
+          Simple App to demo gh-pages deployment!
         </p>
       </div>
     );
